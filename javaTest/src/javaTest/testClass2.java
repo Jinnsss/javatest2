76545bc7caaed2126710a49392f1982db483dcbd
Jinnsss <wkstmrdl93@naver.com>
2023-01-04 18:20:56 +0900

    hello1234

--- javaTest/src/javaTest/testClass2.java
+++ javaTest/src/javaTest/testClass2.java
@@ -4,10 +4,10 @@ public class testClass2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("hello");
-		System.out.print("hello");
-		System.out.print("hello");
-		System.out.print("hello");
+		System.out.print("hello1");
+		System.out.print("hello2");
+		System.out.print("hello3");
+		System.out.print("hello4");
 	}
 
 }
